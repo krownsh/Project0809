efe2c5e83134ca8d18f1aba255af0fdaca082e76
ben0588 <energy95272z@gmail.com>
2022-06-17 13:49:46 +0800

    0617-1349

--- sport/src/App.js
+++ sport/src/App.js
@@ -1,2 +1,17 @@
+import React, { Component } from 'react';
+import NavBar from './components/navBar';
 
-
+class App extends Component {
+    state = { 
+  
+     } 
+    render() { 
+      return (
+        <>
+          <NavBar/> 
+        </>
+      );
+    }
+  }
+   
+  export default App;
